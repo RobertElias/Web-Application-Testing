8e5879dfc4897263ed3f95e36788ba6338c8206c
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-16 13:09:24 -0800

    updates to Dashboard and Display.js

---
+++ web-application-testing/src/components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//display the count of balls and strikes for the at-bat.
+//should be updated when the user records activity on the Dashboard component.
+
+export const Display = (props) => {
+    return(
+        <div className="display-container">
+            <section className='board'>
+
+            </section>
+
+        </div>
+    )
+}
