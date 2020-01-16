09690249e708c76611bcf1c68fccbacd7546417d
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-16 13:18:11 -0800

    Strikes, Balls, Fouls, Hits hanlders completed

--- web-application-testing/src/components/Dashboard.js
+++ web-application-testing/src/components/Dashboard.js
@@ -5,6 +5,37 @@ import React, { useState } from "react";
 //there is NO need to specify the type of hit (single, double, etc).
 //changes recorded on this component should update the information shown by the Display component.
 export const Dashboard = () => {
+    const [strikes, setStrikes] = useState(0);
+    const [balls, setBalls] = useState(0);
+
+
+
+    //Strikes Handler
+    const Strikes = () => {
+        if(strikes < 3) {
+            setStrikes(strikes + 1)
+        };
+    };
+
+    //Ball Handler
+    const Balls = () => {
+        if(balls < 4){
+            setBalls(balls + 1)
+        };
+    };
+
+    //Foul Handler
+    const Fouls = () => {
+        if (strikes < 2) {
+            setStrikes(strikes +1)
+        };
+    };
+
+    //Hit Hanlder
+    const Hits = () => {
+        setStrikes(0)
+        setBalls(0)
+    }
 
     return (
         <div className="dash-container">
