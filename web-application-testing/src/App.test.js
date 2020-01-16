1eb6628d2bdb5a5415b1b4f55d235d60d25ce670
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-16 15:57:34 -0800

    Mvp met

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {Dashboard} from './components/Dashboard'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App renders without crashing', () => {
+  render(<App />);
 });
+
+test('Test that ball count and strike count are rendered on Display', () => {
+  const {getByTestId} = render(<App />);
+
+  getByTestId(/strikeCount/i);
+  
+  getByTestId(/ballCount/i);
+  
+
+})
+
+
