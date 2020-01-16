c887e61b98c7674db56535a66e25a34cdfb1d3e4
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-16 13:20:45 -0800

    Display.js with props completed

--- web-application-testing/src/components/Display.js
+++ web-application-testing/src/components/Display.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 //display the count of balls and strikes for the at-bat.
 //should be updated when the user records activity on the Dashboard component.
 
-export const Display = (props) => {
-    return(
-        <div className="display-container">
-            <section className='board'>
+export const Display = props => {
+  return (
+    <div className="display-container">
+      <section className="board">
+        <h1>Strikes</h1>
+        <div>{props.strikes}</div>
 
-            </section>
-
-        </div>
-    )
-}
+        <h1>Balls</h1>
+        <div>{props.balls}</div>
+      </section>
+    </div>
+  );
+};
